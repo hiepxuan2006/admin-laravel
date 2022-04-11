a3a7ab21fee485a437e985dc390732205e10b8a1
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-04-11 15:58:59 +0700

    make it better

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,9 +10,9 @@
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/AdminLTE/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/AdminLTE/dist/css/adminlte.min.css') }}">
     @yield('css')
 </head>
 
@@ -36,9 +36,9 @@
     <script src="{{ asset('vendors\jquery\jquery-3.6.0.min.js') }}"></script>
     {{-- <script src="{{ asset('') }}AdminLTE/plugins/jquery/jquery.min.js"></script> --}}
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('') }}AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('') }}AdminLTE/dist/js/adminlte.min.js"></script>
+    <script src="{{ asset('AdminLTE/dist/js/adminlte.min.js') }}"></script>
 
     @yield('js')
 
