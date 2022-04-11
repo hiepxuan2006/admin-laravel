64d1d0e8bea360b47cf2ee72850c4d4fc6dc7a63
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-04-11 16:17:09 +0700

    make it better

--- resources/views/admin/menu/indexMenu.blade.php
+++ resources/views/admin/menu/indexMenu.blade.php
@@ -35,8 +35,8 @@
                                         <td>
                                             <a class="btn btn-default"
                                                 href="{{ route('menu.edit', ['id' => $menu->id]) }}">Sửa</a>
-                                            <a href="{{ route('menu.delete', ['id' => $menu->id]) }}"
-                                                class="btn btn-danger">Xóa</a>
+                                                <a data-url="{{ route('menu.delete', ['id' => $category->id]) }}"
+                                                    class="btn btn-danger accessDel">Xóa</a>
                                             {{-- <a data-target="#modal-confirm" data-toggle="modal" type="button"
                                                 class="btn btn-danger">Xóa</a> --}}
                                         </td>
@@ -49,28 +49,12 @@
                     <div class="mx-auto">
                         {{ $menus->links('pagination::bootstrap-4') }}
                     </div>
-                    <div class="modal fade" id="modal-confirm" tabindex="-1" aria-labelledby="exampleModalLabel"
-                        aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Xóa sản phẩm ?</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    Bạn chắc chắn muốn xóa sản phẩm ?
-                                </div>
-                                <div class="modal-footer">
-                                    <a type="button" id="btn-delete-product" class="btn btn-danger">Xóa</a>
-                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Hủy</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+@section('js')
+    <script src="{{ asset('admins\user\index.js') }}"></script>
+    <script src="{{ asset('vendors\sweetalert\sweetalert2@11.js') }}"></script>
 @endsection
