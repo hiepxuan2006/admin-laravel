cdf4fcf493ecb83f930538749b20a34c2cd27332
hiepxuan2006 <hiepxuan2006@gmail.com>
2022-04-13 14:09:09 +0700

    update

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -32,7 +32,7 @@
                   <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                   <li class="nav-item">
-                      <a href="{{ route('categories.index') }}" class="nav-link">
+                      <a href="{{ URL::to('categories.index') }}" class="nav-link">
                           <i class="nav-icon fas fa-th"></i>
                           <p>
                               Danh mục sản phẩm
